66e10af90db546b00ecbe173d0c438bb0bfe36f8
Ali Younis <ali.h.younis@gmail.com>
2015-02-01 17:43:35 -0500

    table view and array all in one scene

--- Classwork/Lesson10/toDoListAllInSameScene/toDoListAllInSameScene/ViewController.swift
+++ Classwork/Lesson10/toDoListAllInSameScene/toDoListAllInSameScene/ViewController.swift
@@ -8,40 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    var toDoListArray: [String] = []
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoListArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        addItem(itemField.text)
+        let(toDo) = toDoListArray[indexPath.row]
+        
+        cell.textLabel?.text = toDo
+        return cell
+    }
     
     @IBOutlet weak var itemField: UITextField!
     
     @IBAction func addItem(sender: AnyObject) {
-        toDoItems.append(itemField.text)
-        
+        toDoListArray.append(itemField.text)
+        println(toDoListArray)
     }
     
     @IBOutlet var toDoList: UITableView!
     
-    var toDoItems: [String] = []
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.toDoList.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
+
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
-        return self.toDoItems.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell:UITableViewCell = self.toDoList.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
-        
-        cell.textLabel?.text = self.toDoItems[indexPath.row]
-        
-        return cell
-    }
-    
-    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
-        println("You selected cell #\(indexPath.row)!")
-    }
+
 
 
 }
