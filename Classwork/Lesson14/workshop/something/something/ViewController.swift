d857e0eb2db05811ae21d3aa013a3059f03540d8
Ali Younis <ali.h.younis@gmail.com>
2015-02-18 20:53:30 -0500

    last class

---
+++ Classwork/Lesson14/workshop/something/something/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  something
+//
+//  Created by Ali Younis on 2/15/15.
+//  Copyright (c) 2015 Ali Younis. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable 
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
